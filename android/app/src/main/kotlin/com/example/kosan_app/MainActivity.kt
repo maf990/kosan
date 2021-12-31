d1106052ef2bca7d23b5098f4473ce5d0b012330
maf990 <abduhmusthafa@gmail.com>
2021-12-31 16:33:43 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/kosan_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kosan_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,90 @@
+import 'package:kosan_app/pages/home_page.dart';
+import 'package:kosan_app/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:kosan_app/pages/login_page.dart';
+
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: whiteColor,
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset('assets/splash_image.png'),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 50,
+                left: 30,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/logo-polosan.png'),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'Find The Best Kos\nto Stay and Happy',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 24,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Stop membuang banyak waktu\nuntuk mencari kos',
+                    style: greyTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    width: 210,
+                    height: 50,
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginPage(),
+                          ),
+                        );
+                      },
+                      color: yellowColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(17),
+                      ),
+                      child: Text(
+                        'Explore Now',
+                        style: whiteTextStyle.copyWith(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
